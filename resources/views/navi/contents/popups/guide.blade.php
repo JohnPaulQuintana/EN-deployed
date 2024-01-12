2ecf339ca809bb618a618818f042d3d0590de521
abirmehedi77 <86663285+abirmehedi77@users.noreply.github.com>
2024-01-12 13:24:55 +0800

    Update guide.blade.php

--- resources/views/navi/contents/popups/guide.blade.php
+++ resources/views/navi/contents/popups/guide.blade.php
@@ -9,7 +9,7 @@
             </div>
             <div class="modal-body gbody">
                 <div class="instruction-section">
-                    <h6><i class="fa fa-solid fa-book"></i> Guide Book</h6>
+                    <h6><i class="fa fa-solid fa-book fa-3x"></i> Guide Book</h6>
                     <p>
                         All the information on how to use is present here.
                     </p>
